e3d09e33c805526b3ec7e990a2ac4118763b0679
ThomasVrd11 <Thomasviard@orange.fr>
2023-11-20 13:08:14 +0100

    some precisions and changes for my binome(il mange)

--- _printf.c
+++ _printf.c
@@ -11,24 +11,40 @@ int _printf(const char *format, ...)
     va_list args;
     int i = 0;
     int length = 0;
+    /*chars to be printed*/
 
     va_start(args, format);
 
+/**
+ * return an error if null
+*/
     if (format == NULL)
     {
         return (-1);
     }
+    /**
+     * iterate over the format string
+    */
     while (format != NULL && format[i]!= '\0')
     {
+    /**
+     * is it a format specifier ???
+     */
         if (format[i] == '%')
         {
             if (format[i + 1] == '\0');
             return (-1);
-
-            length += get_func(args, format[i + 1]);
+            /*+= means = and +, example C += means C = C+A (cc christophe)*/
+        /**
+         * add output length to length
+        */
+            length += selection(args, format[i + 1]);
             i++;
         }
         else
+        /**
+         * print current character and add one to length
+        */
         {
             length += _putchar(format[i]);
             i++;
