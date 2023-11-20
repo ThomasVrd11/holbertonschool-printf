30f674440200f926caf654d16430e1672b3b22d0
christophemr <145457121+christophemr@users.noreply.github.com>
2023-11-20 10:47:14 +0100

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -11,17 +11,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error -1 is returned
- */
-int _putchar(const char c)
-{
-	return (write(1, &c, 1));
-}
